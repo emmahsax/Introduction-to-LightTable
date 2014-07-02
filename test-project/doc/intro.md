f21f3437d7da1590884e458801e2c29ade46eade
Emma Sax <sax.emma.h@gmail.com>
2014-07-02 18:34:49 -0500

    Added test-project to the repository

---
+++ test-project/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to test-project
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test-project/project.clj
@@ -0,0 +1,5 @@
+(defproject test-project "1.0.0-SNAPSHOT"
+  :description "A project for showing basic uses of LightTable and Autoexpect"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [expectations "2.0.6"]]
+  :plugins [[lein-autoexpect "1.0"]])
