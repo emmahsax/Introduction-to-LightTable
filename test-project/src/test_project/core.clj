57f8010e4a62607ff7b7373aeee86746a5f22697
Emma Sax <sax.emma.h@gmail.com>
2014-07-03 11:57:03 -0500

    Added the first movie hashmap

--- test-project/src/test_project/core.clj
+++ test-project/src/test_project/core.clj
@@ -1,6 +1,34 @@
 (ns test-project.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def movie-collection
+  [{:title "Harry Potter and the Sorcerer's Stone" :director #{"Chris Columbus"} :year 2001 :genre #{"Adventure" "Family" "Fantasy"} :length 152 :rating "PG"}
+
+   {:title "Ever After: A Cinderella Story" :director #{"Andy Tennant"} :year 1998 :genre #{"Comedy" "Drama" "Romance"} :length 121 :rating "PG-13"}
+
+   {:title "12 Years a Slave" :director #{"Steve McQueen"} :year 2013 :genre #{"Biography" "Drama" "History"} :length 134 :rating "R"}
+
+   {:title "The Hunger Games" :director #{"Gary Ross"} :year 2012 :genre #{"Adventure" "Sci-Fi"} :length 142 :rating "PG-13"}
+
+   {:title "The Lord of the Rings: The Fellowship of the Ring" :director #{"Peter Jackson"} :year 2001 :genre #{"Adventure" "Action" "Fantasy"} :length 178 :rating "PG-13"}
+
+   {:title "The Incredibles" :director #{"Brad Bird"} :year 2004 :genre #{"Adventure" "Animation" "Action"} :length 115 :rating "PG"}
+
+   {:title "Frozen" :director #{"Chris Buck" "Jennifer Lee"} :year 2001 :genre #{"Adventure" "Family" "Fantasy"} :length 152 :rating "PG"}
+
+   {:title "Argo" :director #{"Ben Affleck"} :year 2012 :genre #{"Biography" "Drama" "History"} :length 120 :rating "R"}
+
+   {:title "Inception" :director #{"Christopher Nolan"} :year 2010 :genre #{"Adventure" "Action" "Mystery"} :length 148 :rating "PG-13"}
+
+   {:title "The Dark Knight" :director #{"Christopher Nolan"} :year 2008 :genre #{"Action" "Drama" "Crime"} :length 152 :rating "PG-13"}
+
+   {:title "The Prestige" :director #{"Christopher Nolan"} :year 2006 :genre #{"Mystery" "Drama" "Thriller"} :length 130 :rating "R"}
+
+   {:title "Aquamarine" :director #{"Elizabeth Allen Rosenbaum"} :year 2006 :genre #{"Comedy" "Family" "Fantasy" "Romance"} :length 104 :rating "PG"}
+
+   {:title "Les Miserables" :director #{"Tom Hopper"} :year 2012 :genre #{"Drama" "Musical" "Romance"} :length 158 :rating "PG-13"}
+
+   {:title "E.T. the Extra-Terrestrial" :director #{"Steven Spielberg"} :year 1982 :genre #{"Adventure" "Family" "Fantasy"} :length 115 :rating "PG"}
+
+   {:title "Schindler's List" :director #{"Steven Spielberg"} :year 1993 :genre #{"Biography" "Drama" "History"} :length 195 :rating "R"}
+
+   {:title "The Big Lebowski" :director #{"Joel Coen" "Ethan Coen"} :year 1998 :genre #{"Comedy" "Crime"} :length 117 :rating "R"}])
